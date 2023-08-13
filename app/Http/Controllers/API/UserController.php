67f64f4c10e1b677f1a057ed84f1cdfcf32de8bf
tapiwa-1 <tmotsi10@gmail.com>
2023-08-13 12:53:37 +0200

    add userController

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserCollection;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     */
+    public function loggedInUser()
+    {
+        try{
+            $user = User::where('id', auth()->user()->id)->get();
+            return response()->json(new UserCollection($user), 200);
+
+        }catch(\Exception $e){
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    // public function updateUserImage(Request $request)
+    // {
+    //     $request->validate(['image' => 'required|mimes:png,jpg,jpeg']);
+    //     if ($request->height === '' || $request->width === '' || $request->top === '' || $request->left === '') {
+    //         return response()->json(['error' => 'The dimensions are incomplete'], 400);
+    //     }
+
+    //     try {
+    //         $user = (new FileService)->updateImage(auth()->user(), $request);
+    //         $user->save();
+
+    //         return response()->json(['success' => 'OK'], 200);
+    //     } catch (\Exception $e) {
+    //         return response()->json(['error' => $e->getMessage()], 400);
+    //     }
+    // }
+
+    public function getUser($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            return response()->json([
+                'success' => 'OK',
+                'user' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function updateUser(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+
+        try {
+            $user = User::findOrFail(auth()->user()->id);
+
+            $user->name = $request->input('name');
+            $user->bio = $request->input('bio');
+            $user->save();
+
+            return response()->json(['success' => 'OK'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/logged-in-user', [UserController::class, 'loggedInUser']);
+    Route::post('/update-user-image', [UserController::class, 'updateUserImage']);
+    Route::patch('/update-user', [UserController::class, 'updateUser']);
 });
